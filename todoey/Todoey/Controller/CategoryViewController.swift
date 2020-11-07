bd5931eb118e0ae1a5463bf2b358118133bfcfe8
Alex Ho <huyho114@gmail.com>
2020-11-06 23:51:55 -0500

    update todoey, continue challange

---
+++ todoey/Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Alex Ho on 10/28/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    var category = [Category]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    //MARK: - TableView Datasource Methods
+   
+
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: -  Add New Category
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    
+}
