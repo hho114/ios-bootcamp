786ae4ade005b8698c226efae695d348f8500b6e
Alex Ho <huyho114@gmail.com>
2021-04-19 13:04:43 -0400

    update realm for todoey

--- todoey/Todoey/AppDelegate.swift
+++ todoey/Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("didFinishLaunchingWithOptions")
+        print(Realm.Configuration.defaultConfiguration.fileURL ?? "url n/a")
+        let data = Data()
+        data.name = "Alex"
+        data.age = 15
+        
+        do{
+            let realm = try Realm()
+            try realm.write{
+                realm.add(data)
+            }
+            
+        }catch{
+            print("Error install realmswift \(error)")
+        }
+        
+        
+        
+        
+        
         return true
     }
 
---
+++ todoey/Todoey/Data Model/Data.swift
@@ -0,0 +1,17 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Alex Ho on 4/19/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+    
+}
